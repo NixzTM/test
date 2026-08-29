0a56751a09e2fbc9dc1c13332e913b9dcc790376
NixzTM <luckyjumparena@gmail.com>
2026-08-29 09:29:48 +0200

    Bump ArenaControl to v2.5

--- ArenaControl/app/build.gradle.kts
+++ ArenaControl/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.arenacommunity.control"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.4.0"
+        versionCode = 8
+        versionName = "2.5.0"
     }
 
     buildTypes {
